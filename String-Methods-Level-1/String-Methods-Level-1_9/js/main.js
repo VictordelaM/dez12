b6be46349a77ad0f512f113b065167cea2650431
VictordalaM <delamotte.v@gamil.com>
2023-12-13 03:51:18 +0100

    fertig

--- String-Methods-Level-1/String-Methods-Level-1_9/js/main.js
+++ String-Methods-Level-1/String-Methods-Level-1_9/js/main.js
@@ -1,6 +1,17 @@
-const text1 = "Sam is going to school"
-const text2 = "Susi" 
-const text3 = "and"
+const Sam = "Sam is going to school"
+const Susi = "Susi" 
+const and = "and"
 
-document.write(text1.concat(" and to the movie theater" + "<br>"))
-document.write(text2.concat(" " + text3 + " " + text1))
+document.write(Sam.concat( " " + and + " to the movie theater" + "<br>"))
+document.write(Sam.concat(" the movie theater"+ "<br>"))
+document.write(Susi.concat(" " + and + " " + Sam.replace("is", "are") + "<br>"))
+document.write(Susi.concat(" " + and + " " + Sam.slice(0,4) +"are"+ Sam.slice(7,15) + " gym "+ and+Sam.slice(12,16) +"the movie theater"+"<br>"))
+document.write(Susi.concat(Sam.slice(3) + " " + and +Sam.slice(12,16)+ "the movie theater"+ "<br>"))
+
+
+
+// Sam is going to school and to the movie theater
+// Sam is going to the movie theater
+// Susi and Sam are going to school
+// Susi and Sam are going to gym and to the movie theater
+// Susi is going to school and to the movie theater
